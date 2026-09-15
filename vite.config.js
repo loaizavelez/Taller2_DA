296317ea91b706178075bdd5b3eb26173f6ee759
loaizavelez <158523231+loaizavelez@users.noreply.github.com>
2026-09-15 04:33:08 +0000

    configuracion vite

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import babel from '@rolldown/plugin-babel'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
+  // ¡Aquí agregamos la línea clave para GitHub Pages!
+  base: '/Taller2_DA/',
   plugins: [
     tailwindcss(),
     react(),
